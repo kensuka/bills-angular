89c53eaa77d4dcba9916d47148374bcf356b8c73
Kenji Mukai <kensuka21@gmail.com>
2018-07-04 21:57:23 -0400

    Update app.js

--- public/app.js
+++ public/app.js
@@ -1,6 +1,3 @@
-/**
- * Created by kmukai on 9/15/2016.
- */
 (function () {
     angular.module('bills', ['ui.router', 'ui.materialize', 'chart.js'])
         .constant('baseUrl', 'http://localhost:8080')
